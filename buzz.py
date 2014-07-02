7302d7f82a6b443df8a8fd8bddec8da4f2afea32
CoolAcid <jakendall@gmail.com>
2014-07-02 13:36:39 -0400

    Code cleanup

--- buzz.py
+++ buzz.py
@@ -36,28 +36,45 @@ class buzz:
 	#    dev.attach_kernel_driver(self.interface)
 
     def setlights(self, control):
-	print "setting lights"
+	# Sets lights based on binaray
+	# 1 = Controller 1
+	# 2 = Controller 2
+	# 4 = Controller 3
+	# 8 = Controller 4
+
 	self.lights[0] = 0xFF if control & 1 else 0x00
 	self.lights[1] = 0xFF if control & 2 else 0x00
 	self.lights[2] = 0xFF if control & 4 else 0x00
 	self.lights[3] = 0xFF if control & 8 else 0x00
 	self.device.ctrl_transfer(0x21, 0x09, 0x0200,0,[0x0,self.lights[0],self.lights[1],self.lights[2],self.lights[3],0x0,0x0])
 
-    def readcontroller(self):
+    def fliplight(self):
+	# Flips the state of a controllers light
+	# TODO
+	pass
+
+    def readcontroller(self, raw = False):
+	# Reads the controller
+	# Returns the result of Parsecontroller (the changed bit) or raw
 	try: 
 	    cfg = self.device.get_active_configuration()
 	    self.endpoint = cfg[(0,0)][0]
 	    data = self.device.read(self.endpoint.bEndpointAddress, self.endpoint.wMaxPacketSize)
+	    parsed = self.parsecontroller(data)
 	except usb.core.USBError as e: 
 	    if e[0] != 110:
 		traceback.print_exc(file=sys.stdout)
 		# TODO: Should probably raise an error here, as it's something unexpected.
 	    data = None
+	if data != None and raw == False:
+	    data = parsed
 	return data
 
     def parsecontroller(self, data):
 	# Function to parse the results of readcontroller
 	# We break this out incase someone else wants todo something different
+	# Returns the changed bit
+
 	# Controller 1
 	self.buttons[0]["red"] =    True if data[2] & 1 else False
 	self.buttons[0]["yellow"] = True if data[2] & 2 else False
@@ -65,29 +82,37 @@ class buzz:
 	self.buttons[0]["orange"] = True if data[2] & 8 else False
 	self.buttons[0]["blue"] =   True if data[2] & 16 else False
 
+	# Controller 2
 	self.buttons[1]["red"] =    True if data[2] & 32 else False
 	self.buttons[1]["yellow"] = True if data[2] & 64 else False
 	self.buttons[1]["green"] =  True if data[2] & 128 else False
 	self.buttons[1]["orange"] = True if data[3] & 1 else False
 	self.buttons[1]["blue"] =   True if data[3] & 2 else False
 
+	# Controller 3
 	self.buttons[2]["red"] =    True if data[3] & 4 else False
 	self.buttons[2]["yellow"] = True if data[3] & 8 else False
 	self.buttons[2]["green"] =  True if data[3] & 16 else False
 	self.buttons[2]["orange"] = True if data[3] & 32 else False
 	self.buttons[2]["blue"] =   True if data[3] & 64 else False
 
+	# Controller 4
 	self.buttons[3]["red"] =    True if data[3] & 128 else False
 	self.buttons[3]["yellow"] = True if data[4] & 1 else False
 	self.buttons[3]["green"] =  True if data[4] & 2 else False
 	self.buttons[3]["orange"] = True if data[4] & 4 else False
 	self.buttons[3]["blue"] =   True if data[4] & 8 else False
 
+	# TODO: Track and report what changed. 
 	return self.buttons
 
-    def readlights(self):
-	# TODO: Should return what the state of self.lights
-	print (self.controller.leds(verbose=True))
+    def getbuttons(self):
+	# Returns current state of buttons
+	return self.buttons
+
+    def getlights(self):
+	# Returns the current state of the lights
+	return self.lights
 	
 
 if __name__=='__main__':
@@ -95,10 +120,9 @@ if __name__=='__main__':
 #    for x in range(16):
 #	buzz.setlights(x)
 #	time.sleep(1)
-    buzz.setlights(8)
+    buzz.setlights(0)
     while True:
-	r = buzz.readcontroller()
+	r = buzz.readcontroller(True)
 	if r != None:
-	    q = buzz.parsecontroller(r)
 	    print r
-	    print q
+	    print buzz.getbuttons()
